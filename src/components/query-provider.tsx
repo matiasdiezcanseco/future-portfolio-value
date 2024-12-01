4b0abf2e8f556d7cd68c7e80717cc14dc8901245
Matias <matiasdiezcanseco@gmail.com>
2024-12-01 12:02:34 -0500

    removed persist to fix hydration errors, will fix later

--- src/components/query-provider.tsx
+++ src/components/query-provider.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
-import { QueryClient } from '@tanstack/react-query';
-import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState } from 'react';
 
 export const ReactQueryClientProvider = ({ children }: { children: React.ReactNode }) => {
@@ -20,13 +18,5 @@ export const ReactQueryClientProvider = ({ children }: { children: React.ReactNo
       }),
   );
 
-  const persister = createSyncStoragePersister({
-    storage: window.localStorage,
-  });
-
-  return (
-    <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
-      {children}
-    </PersistQueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
